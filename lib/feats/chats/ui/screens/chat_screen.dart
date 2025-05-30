f027d25a58f628499bc62ea7f5ce08d0c2f408aa
Meshooo <mahmoudabdaltwab57@gmail.com>
2025-05-30 18:56:24 +0300

    adding some notification functionality

--- lib/feats/chats/ui/screens/chat_screen.dart
+++ lib/feats/chats/ui/screens/chat_screen.dart
@@ -1,11 +1,35 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:free_chat/feats/auth/helpers/firebase_helpers.dart';
 import 'package:free_chat/feats/auth/helpers/shared_pref.dart';
 import 'package:free_chat/feats/chats/ui/widgets/chat_messages.dart';
 import 'package:free_chat/feats/chats/ui/widgets/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotification();
+  }
+
+  void setupPushNotification() async {
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();
+
+    fcm.subscribeToTopic('chat');
+
+    final token = await fcm.getToken();
+
+    print('token:           $token');
+  }
 
   @override
   Widget build(BuildContext context) {
